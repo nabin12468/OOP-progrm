0412d99f725a537cc6c66d428166e734c07c056f
nabin12468 <nabindamase12345@gmail.com>
2025-07-31 14:17:05 +0545

    esception handling

--- 410.cpp
+++ 410.cpp
@@ -1,8 +1,58 @@
-//multiple inheritance
+// multiple inheritance
 #include <iostream>
 using namespace std;
 
-int main() {
-        
+
+
+
+class students{
+    protected:
+     int roll_number;
+     public:
+     void set_roll_number(int);
+     void get_roll_number(void);
+     
+};
+
+void student :: set_roll_number(int r)
+{
+    roll_number = r;
+}
+void student :: get_roll_number(int r)
+{
+    cout << "The roll number is: " << roll_number << endl;
+}
+
+
+int main()
+{
+
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ For protected member:
+                    public derivation  private derivation  protected derivation
+
+   1.private members  not inherited       not inherited        not inherited
+   2.protected members  protected         private              protected
+   3.public members      public              private              protected
+
+
+
+*/
