53c97f522ce699148c349840b3ab9a7d6aeedc34
nabin12468 <nabindamase12345@gmail.com>
2025-08-03 17:33:48 +0545

    overloading function

--- 420.cpp
+++ 420.cpp
@@ -1,11 +1,23 @@
+// overloading function with different number of arguments
 #include <iostream>
 using namespace std;
 
+int area(int l,int b)
+{
+    return l+b;
+
+}
+int area (int l)
+{
+    return l*l;
+}
 int main() {
-    int a,b,c;
- a=2,b=4;
- c=a+b;
- 
- 
+    int Sa,ra,l,b;
+    cout<<"enter length and breath of rectangle "<<endl;
+    cin>>l>>b;
+    ra=area(l,b);
+    cout<<"area of rectangle="<<ra<<endl;
+    cout<<"area of square ="<<Sa<<endl;
+    
     return 0;
 }
