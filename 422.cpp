13ee005e8abc5e2866b9fe02e475c244122f3945
nabin12468 <nabindamase12345@gmail.com>
2025-08-07 13:18:40 +0545

    hello world

--- 422.cpp
+++ 422.cpp
@@ -1,7 +1,13 @@
+// Necessary header files for input output functions
 #include <iostream>
 using namespace std;
 
+
+
 int main() {
-    
+  
+                 // This statement prints "Hello World"
+    cout << "Hello World";
+
     return 0;
 }
