3a5342bf2ff5e356eaee294baba80edcf1b545e6
nabin12468 <nabindamase12345@gmail.com>
2025-08-12 10:29:43 +0545

    exception

--- 436.cpp
+++ 436.cpp
@@ -1,3 +1,7 @@
+//exception handlinggit
+
+
+
 #include <iostream>
 using namespace std;
 
@@ -6,6 +10,18 @@ class customer{
 };
 
 int main() {
-    
+    int a,b;
+    cin>>a>>b;
+
+    try{
+        if(b==0)
+        throw "divided by zero is not possible";
+    int c=a/b;
+    cout<<c<<endl;
+    }
+    catch(const char *e)
+    {
+        cout<<"exception occured:"<<e<<endl;
+    }
     return 0;
 }
