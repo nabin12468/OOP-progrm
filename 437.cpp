d3a37aa14ec323d85930522b46725b684dd917cc
nabin12468 <nabindamase12345@gmail.com>
2025-08-13 14:01:27 +0545

    template

---
+++ 437.cpp
@@ -0,0 +1,26 @@
+//templates
+
+#include <iostream>
+using namespace std;
+
+class vector{
+    int * arr;
+    int size;
+    public:
+    vector(int m){
+        size=m;
+        arr = new int[size];
+    }
+    int dotProduct(vector &v){
+int d=0;
+for(int i=0;i<size;i++)
+{
+    
+}
+    }
+};
+
+int main() {
+    
+    return 0;
+}
