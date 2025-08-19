f8ad2b8f0b2cc3d3cd4d52f614bba382ab644849
nabin12468 <nabindamase12345@gmail.com>
2025-08-19 14:35:00 +0545

    operator

---
+++ 440.cpp
@@ -0,0 +1,22 @@
+//operator overloading
+//overload + operator
+
+#include <iostream>
+using namespace std;
+class demo
+{
+    int a;
+public:
+    void getdata(){
+        cout << "Enter a number: ";
+        cin >> a;
+    }
+    void putdata(){
+        cout << "The number is: " << a << endl;
+    }
+};
+
+int main() {
+    
+    return 0;
+}
