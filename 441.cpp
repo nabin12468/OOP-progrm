fbbe6a690786bf21b308ef4bf89e9be1f9c836f9
nabin12468 <nabindamase12345@gmail.com>
2025-08-21 14:44:18 +0530

    ambiguity in multiple inheritance

--- 441.cpp
+++ 441.cpp
@@ -1,8 +1,34 @@
-
+//ambiguity
 #include <iostream>
 using namespace std;
 
+class A{
+    public:
+    void show(){
+    cout<< "Hello A" << endl;
+    }
+};
+class B{
+    public:
+    void show (){
+        cout<< "Hello B" << endl;
+
+    }
+};
+class c :public A, public B {
+    public:
+    void show() {
+        A :: show();
+    }
+    };
+
 int main() {
-    
+    A aobj;
+    B bobj;
+    aobj.show();
+    bobj.show();
+    c c;
+    c.show(); // Calls A's show method due to the implementation in class c
+
     return 0;
 }
