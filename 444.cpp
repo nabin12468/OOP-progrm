883f534c0621a0d71ad2de442a1f85fd6678b669
nabin12468 <nabindamase12345@gmail.com>
2025-08-27 10:54:42 +0545

    file handling

---
+++ 444.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+    string st="Harry bhai";
+    // opening files using constructor and writing it
+    string st2;
+
+   // ofstream out("sample.txt");  //write operation
+   // out<<st;
+ ifstream in ("sample.xtx"); //read operation
+ getline(in,st2);
+ cout<<st2;
+    return 0;
+}
