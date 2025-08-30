63206015dd5689cdd93dc05b7c8026945e8623c4
nabin12468 <nabindamase12345@gmail.com>
2025-08-30 16:01:44 +0545

    call by pointer

---
+++ 446.cpp
@@ -0,0 +1,22 @@
+//call by pointer or pass by address
+
+#include <iostream>
+using namespace std;
+
+void passbyvalue(int*x, int*y) {
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+int main() {
+    int a = 10, b = 20;
+    cout << "Before swapping: a = " << a << ", b = " << b
+        << endl;
+        passbyvalue(&a, &b);
+    cout << "After swapping: a = " << a << ", b = " << b
+        << endl;
+    
+    return 0;
+}
