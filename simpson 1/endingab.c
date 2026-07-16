728c22e933fc09ea44d167154e0cfb4118c4cbd3
nabin12468 <nabindamase12345@gmail.com>
2026-07-16 15:38:59 +0545

    ending withab

---
+++ simpson 1/endingab.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int state = 0;
+
+    printf("Enter string: ");
+    scanf("%s", str);
+
+    for (int i = 0; i < strlen(str); i++) {
+        switch(state) {
+            case 0:
+                if(str[i]=='a')
+                    state=1;
+                else if(str[i]=='b')
+                    state=0;
+                else {
+                    printf("Invalid Input\n");
+                    return 0;
+                }
+                break;
+
+            case 1:
+                if(str[i]=='a')
+                    state=1;
+                else if(str[i]=='b')
+                    state=2;
+                else {
+                    printf("Invalid Input\n");
+                    return 0;
+                }
+                break;
+
+            case 2:
+                if(str[i]=='a')
+                    state=1;
+                else if(str[i]=='b')
+                    state=0;
+                else {
+                    printf("Invalid Input\n");
+                    return 0;
+                }
+                break;
+        }
+    }
+
+    if(state==2)
+        printf("Accepted\n");
+    else
+        printf("Rejected\n");
+
+    return 0;
+}
