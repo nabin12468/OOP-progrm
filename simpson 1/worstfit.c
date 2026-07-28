1da69e562652b3ad4ec4f6e3acf2934f7cea7095
nabin12468 <nabindamase12345@gmail.com>
2026-07-28 21:13:02 +0545

    worstfit

--- simpson 1/worstfit.c
+++ simpson 1/worstfit.c
@@ -44,16 +44,4 @@ int main() {
         }
     }
 
-    printf("\nProcess\tSize\tBlock\n");
-
-    for (int i = 0; i < np; i++) {
-        printf("P%d\t%d\t", i + 1, process[i]);
-
-        if (allocation[i] != -1)
-            printf("%d\n", allocation[i] + 1);
-        else
-            printf("Not Allocated\n");
-    }
-
-    return 0;
-}
+    
